d34dac954bf05518227ad5ffa77d5fea0475ecb9
DCartist <pbannerman13@gmail.com>
2019-05-17 10:10:17 -0400

    new changes

---
+++ src/js/main.js
@@ -0,0 +1,16 @@
+// import foo from "../*";
+// {
+//   'file-1': '/file-1.8e73c985.png',
+//   'file-2': '/file-1.8e73c985.png'
+// }
+
+/* var jade = require('pug'),
+    fs = require('fs'),
+    path = require('path');
+
+var fn = jade.compile(fs.readFileSync('index.pug', 'utf-8'), {
+    filename: path.join(__dirname, 'index.pug'),
+    // pretty: true
+});
+
+console.log(fn()); */
